7cbd5df573ebc1fdea530692c002297177dd6b5b
jorgemanzo <jmanzo203689@gmail.com>
2020-08-01 16:47:39 -0700

    Usable with resizable textarea

--- src/components/codeView.js
+++ src/components/codeView.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react';
-
+import TextareaAutosize from 'react-textarea-autosize';
 const CodeView = (props) => {
     let [code, setCode] = useState('')
-    let [lines, setLines] = useState([{lineNum : 1}, {lineNum : 2}, {lineNum : 3}])
+    let [lines, setLines] = useState([1])
 
     const onCodeChange = (event) => {
         setCode(event.target.value)
-        console.log(event.target.value)
-        let num = code.split(/\n/).length
-        console.log("Line count: ", num)
-        let tempLines = []
-        for(let j = 0; j < num; j++) {
-            tempLines.push({lineNum : j+1})
-        }
+        let num = event.target.value.split(/\n/).length
         if(num === 0) {
-            tempLines.push({lineNum : 1})
+            setLines([1])
+        } else {
+            let temp = []
+            for(let i = 0; i < num; i++) {
+                temp.push(i+1)
+            }
+            setLines(temp)
         }
-        setLines(tempLines)
     }
     return (
         <div className="code">
             <div className="codeArea" >
                 <span className="line-number">
                     {
-                        lines.map((line, index) => <span key={index}> {line.lineNum} </span>)
+                        lines.map((line) => <span className="minispan" key={line}> {line} </span>)
                     }
                 </span>
-                <textarea value={code} onChange={onCodeChange} className="noStyleTextArea"/>
+                <TextareaAutosize value={code} onChange={onCodeChange} className="noStyleTextArea"/>
                 <span className="cl"></span>
             </div>
             <button className="codeButton" >Copy</button>
